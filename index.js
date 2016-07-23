35b591189b170e7c78dfa91e64e6dcb8423c2892
Falciron <apmattso@gmail.com>
2016-07-23 12:12:05 -0600

    Tune Everything Else Out

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
 'use strict';
 
-var Botkit = require('botkit')
+var Botkit = require('botkit');
 var querystring = require('querystring');
 var request = require('request');
 require('q');
 
-var token = process.env.SLACK_TOKEN
+// Default Botkit connection code.
+var token = process.env.SLACK_TOKEN;
 
 var controller = Botkit.slackbot({
-  // reconnect to Slack RTM when connection goes bad
   retry: Infinity,
   debug: false
-})
+});
 
-// Assume single team mode if we have a SLACK_TOKEN
 if (token) {
-  console.log('Starting in single-team mode')
   controller.spawn({
     token: token
   }).startRTM(function (err, bot, payload) {
     if (err) {
-      throw new Error(err)
+      throw new Error(err);
     }
-
-    console.log('Connected to Slack RTM')
-  })
-// Otherwise assume multi-team mode - setup beep boop resourcer connection
+  });
 }
 
 // Capitalize the search phrase in order to better match Gatherer's format.
@@ -41,28 +36,19 @@ function evaluateCard(card,searchTerm){
   else return false;
 }
 
-controller.on('message_received', function(bot, message) {
-  var bracketRegex = /\[(.*?)\]/g;
-  if (message !== null) {
-    var searchTermList = message.map(function (message) {
-      var tempMessage = message;
-      tempMessage = tempMessage.replace(/\[|\]/g, '');
-      return tempMessage;
-    });
-    for (index = 0; index < searchTermList.length; index++){
-      getCardImage(searchTermList[index],message);
-    }
-  }
+controller.hears('\[(.*?)\]', function(bot, message) {
+	// Zero index is the entire message, so start at one.
+	for (var index = 1; index < message.match.length; index++){
+		getCardImage(message.match[index], bot, message);
+	}
 });
 
-function getCardImage(searchTerm,message) {
-	searchTerm = searchTerm.replace(/\w\S*/g, titleCase);
-	searchTerm = querystring.stringify({name: searchTerm});
-	request('https://api.magicthegathering.io/v1/cards?' + searchTerm, function (err, res, body) {
+function getCardImage(searchTerm, bot, message) {
+	request('https://api.magicthegathering.io/v1/cards?' + querystring.stringify({name: searchTerm}), function (err, res, body) {
 		if (!err && res.statusCode === 200){
 			var cards = JSON.parse(body);
 			if (cards.cards.length === 0){
-				bot.reply(message, 'The card "' + element + '" could not be found.');
+				bot.reply(message, 'The card "' + searchTerm + '" could not be found.');
 			} else {
 				var exactMatchFound = false;
 				var firstImageFound = false;
@@ -70,7 +56,7 @@ function getCardImage(searchTerm,message) {
 				var bestMatchIndex = 0;
 				var cardCounter = 0;
 				while (!exactMatchFound && cardCounter < cards.cards.length){
-					var evalResult = evaluateCard(cards.cards[cardCounter],element);
+					var evalResult = evaluateCard(cards.cards[cardCounter],searchTerm);
 					if (evalResult === 'ExactMatch'){
 						bestMatchIndex = cardCounter;
 						exactMatchFound = true;
