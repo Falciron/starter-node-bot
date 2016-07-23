1dd429482864331658c0b0c65097836ce8b86aaf
Falciron <apmattso@gmail.com>
2016-07-22 19:51:16 -0700

    Added the Magic

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+'use strict';
+
 var Botkit = require('botkit')
+var querystring = require('querystring');
+var request = require('request');
+require('q');
 
 var token = process.env.SLACK_TOKEN
 
@@ -21,56 +26,66 @@ if (token) {
     console.log('Connected to Slack RTM')
   })
 // Otherwise assume multi-team mode - setup beep boop resourcer connection
-} else {
-  console.log('Starting in Beep Boop multi-team mode')
-  require('beepboop-botkit').start(controller, { debug: true })
 }
 
-controller.on('bot_channel_join', function (bot, message) {
-  bot.reply(message, "I'm here!")
-})
+// Capitalize the search phrase in order to better match Gatherer's format.
+function titleCase(str){
+	return str.charAt(0).toUpperCase() + str.substr(1).toLowerCase();
+}
 
-controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
-  bot.reply(message, 'Hello.')
-})
+// Determine if a) the card has an image and b) the card's name matches the search phrase exactly.
+function evaluateCard(card,searchTerm){
+	if (card.hasOwnProperty('imageUrl')){
+		return (card.name === searchTerm) ? 'ExactMatch' : 'Candidate';
+	}
+  else return false;
+}
 
-controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
-  bot.reply(message, 'Hello.')
-  bot.reply(message, 'It\'s nice to talk to you directly.')
-})
+controller.on('message_received', function(bot, message) {
+  var bracketRegex = /\[(.*?)\]/g;
+  if (result !== null) {
+    var searchTermList = result.map(function (result) {
+      var tempResult = result;
+      tempResult = tempResult.replace(/\[|\]/g, '');
+      return tempResult;
+    });
+    for (index = 0; index < searchTermList.length; index++){
+      getCardImage(searchTermList[index],message);
+    }
+  }
+});
 
-controller.hears('.*', ['mention'], function (bot, message) {
-  bot.reply(message, 'You really do care about me. :heart:')
-})
-
-controller.hears('help', ['direct_message', 'direct_mention'], function (bot, message) {
-  var help = 'I will respond to the following messages: \n' +
-      '`bot hi` for a simple message.\n' +
-      '`bot attachment` to see a Slack attachment message.\n' +
-      '`@<your bot\'s name>` to demonstrate detecting a mention.\n' +
-      '`bot help` to see this again.'
-  bot.reply(message, help)
-})
-
-controller.hears(['attachment'], ['direct_message', 'direct_mention'], function (bot, message) {
-  var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
-  var attachments = [{
-    fallback: text,
-    pretext: 'We bring bots to life. :sunglasses: :thumbsup:',
-    title: 'Host, deploy and share your bot in seconds.',
-    image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
-    title_link: 'https://beepboophq.com/',
-    text: text,
-    color: '#7CD197'
-  }]
-
-  bot.reply(message, {
-    attachments: attachments
-  }, function (err, resp) {
-    console.log(err, resp)
-  })
-})
-
-controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
-  bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
-})
+function getCardImage(searchTerm,message) {
+	searchTerm = searchTerm.replace(/\w\S*/g, titleCase);
+	searchTerm = querystring.stringify({name: searchTerm});
+	request('https://api.magicthegathering.io/v1/cards?' + searchTerm, function (err, res, body) {
+		if (!err && res.statusCode === 200){
+			var cards = JSON.parse(body);
+			if (cards.cards.length === 0){
+				bot.reply(message, 'The card "' + element + '" could not be found.');
+			} else {
+				var exactMatchFound = false;
+				var firstImageFound = false;
+				var firstImageIndex = 0;
+				var bestMatchIndex = 0;
+				var cardCounter = 0;
+				while (!exactMatchFound && cardCounter < cards.cards.length){
+					var evalResult = evaluateCard(cards.cards[cardCounter],element);
+					if (evalResult === 'ExactMatch'){
+						bestMatchIndex = cardCounter;
+						exactMatchFound = true;
+					} else if (evalResult === 'Candidate'){
+						firstImageIndex = cardCounter;
+						firstImageFound = true;
+					}
+					cardCounter++;
+				}
+				if (exactMatchFound){
+					bot.reply(message, cards.cards[bestMatchIndex].imageUrl);
+				} else if (firstImageFound){
+					bot.reply(message, cards.cards[firstImageIndex].imageUrl);
+				}
+			}
+		}
+	});
+}
