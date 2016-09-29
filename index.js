fe923b32970de4211e3085212af3ec71005d4f36
Falciron <apmattso@gmail.com>
2016-09-28 21:51:08 -0500

    Too Smart for Its Own Good
    Utilize the new mtgsdk module to access the magicthegathering.io API
    and fix the smart quote bug that prevented apostrophes from matching in
    search terms.

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
 'use strict';
 
-var Botkit = require('botkit');
-var querystring = require('querystring');
-var request = require('request');
-require('q');
+const Botkit = require('botkit');
+const mtg = require('mtgsdk');
 
 // Default Botkit connection code.
 var token = process.env.SLACK_TOKEN;
 
 var controller = Botkit.slackbot({
-  retry: Infinity,
-  debug: false
+	retry: Infinity,
+	debug: false
 });
 
 if (token) {
-  controller.spawn({
-    token: token
-  }).startRTM(function (err, bot, payload) {
-    if (err) {
-      throw new Error(err);
-    }
-  });
-}
-
-// Capitalize the search phrase in order to better match Gatherer's format.
-function titleCase(str){
-	return str.charAt(0).toUpperCase() + str.substr(1).toLowerCase();
+	controller.spawn({
+		token: token
+	}).startRTM(function (err) {
+		if (err) {
+			throw new Error(err);
+		}
+	});
 }
 
 // Determine if a) the card has an image and b) the card's name matches the search phrase exactly.
@@ -33,31 +26,31 @@ function evaluateCard(card,searchTerm){
 	if (card.hasOwnProperty('imageUrl')){
 		return (card.name === searchTerm) ? 'ExactMatch' : 'Candidate';
 	}
-  else return false;
+	else return false;
 }
 
 controller.hears([/\[(.*?)\]/g], ['direct_mention','direct_message','mention','ambient'], function(bot, message) {
 	for (var index = 0; index < message.match.length; index++){
 		var searchTerm = message.match[index].replace(/[[\]]/g,'');
-		getCardImage(searchTerm,bot,message);
+		var notSoSmartTerm = searchTerm.replace(/’/g, "'");
+		getCardImage(notSoSmartTerm,bot,message);
 	}
 });
 
-function getCardImage(searchTerm,bot,message) {
-	request('https://api.magicthegathering.io/v1/cards?' + querystring.stringify({name: searchTerm}), function (err, res, body) {
-		if (!err && res.statusCode === 200){
-			var cards = JSON.parse(body);
-			if (cards.cards.length === 0){
+function getCardImage(searchTerm,bot,message){
+	mtg.card.where({name: searchTerm})
+		.then(function(cards){
+			if (cards.length === 0){
+				console.log(searchTerm);
 				bot.reply(message,'The card "' + searchTerm + '" could not be found.');
 			} else {
-				var exactMatchFound = false;
 				var firstImageFound = false;
 				var firstImageIndex = 0;
 				var cardCounter = 0;
-				while (cardCounter < cards.cards.length){
-					var evalResult = evaluateCard(cards.cards[cardCounter],searchTerm);
+				while (cardCounter < cards.length){
+					var evalResult = evaluateCard(cards[cardCounter],searchTerm);
 					if (evalResult === 'ExactMatch'){
-						bot.reply(message,cards.cards[cardCounter].imageUrl);
+						bot.reply(message,cards[cardCounter].imageUrl);
 						return;
 					} else if (evalResult === 'Candidate'){
 						firstImageIndex = cardCounter;
@@ -66,16 +59,13 @@ function getCardImage(searchTerm,bot,message) {
 					cardCounter++;
 				}
 				if (firstImageFound){
-					bot.reply(message,cards.cards[firstImageIndex].imageUrl);
-					return;
+					bot.reply(message,cards[firstImageIndex].imageUrl);
 				} else {
 					bot.reply(message,'No card image could be found.');
-					return;
 				}
 			}
-		} else {
+		})
+		.catch(function(){
 			bot.reply(message,'Something went wrong. Please try again later.');
-			return;
-		}
-	});
+		});
 }
